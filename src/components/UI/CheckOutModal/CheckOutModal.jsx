f13c0d3a82ef142ca4b9dfda01f6baad1675d462
abdisalomoff <Avazbek6757@gmail.com>
2024-02-02 18:34:39 +0500

    add modal

--- src/components/UI/CheckOutModal/CheckOutModal.jsx
+++ src/components/UI/CheckOutModal/CheckOutModal.jsx
@@ -14,9 +14,9 @@ const CheckoutModal = ({ isOpen, handleClose }) => {
       <Fade in={isOpen}>
         <div className="bg-white p-6 rounded-md">
           <div className='bg-orange-200 w-[400px]'>
-          <h2 className="text-2xl text-center font-semibold mb-4">Zakasingiz tasdiqlandi</h2>
+          <h2 className="text-2xl text-center font-semibold mb-4">Your payment confirmed</h2>
           <Button className='mx-auto' variant="contained" color="primary" onClick={handleClose}>
-            Yopish
+            Close
           </Button>
           </div>
         </div>
